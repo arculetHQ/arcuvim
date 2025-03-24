6fbf176bcc4b3c75b92af323145603caa5016de7
Asaduzzaman Atik <asadatik.dev@gmail.com>
2025-03-25 00:04:07 +0600

    ok (#9)

--- lua/arcuvim/plugins/configs/gitlinker.lua
+++ lua/arcuvim/plugins/configs/gitlinker.lua
@@ -1,10 +1,9 @@
 local M = {}
 
 function M.keys()
-    return {
-        { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
-        { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
-    }
+	return {
+		{ "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+	}
 end
 
 return M
